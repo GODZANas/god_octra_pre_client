086985f210888012a91a4247295efe68e81bed41
GODSANAS <asgodsangod@gmail.com>
2025-07-20 15:52:11 +0000

    pushing

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ if ! command -v python3 &>/dev/null; then echo "python3 not found"; exit 1; fi
 [ ! -d "venv" ] && python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python cli.
+python cli.cp wallet.json.example wallet.json
